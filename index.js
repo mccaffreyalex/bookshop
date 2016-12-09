be97592dbe8bee07fd2714b94298da5b825d5166
Alex McCaffrey <mccaffra@uni.coventry.ac.uk>
2016-12-09 23:47:41 +0000

    port change

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ server.use(restify.authorizationParser())
 
 const bookshop = require('./bookshop.js')
 const status = {
-	ok: 200,
-	added: 201,
-	badRequest: 400
+    ok: 200,
+    added: 201,
+    badRequest: 400
 }
-const defaultPort = 8080
+const defaultPort = 80
 
 server.get('/', (req, res, next) => {
-	res.redirect('/books', next)
+    res.redirect('/books', next)
 })
 
 /**
@@ -27,64 +27,64 @@ server.get('/', (req, res, next) => {
  * @apiParam {String} q Query string
  */
 server.get('/books', (req, res) => {
-	bookshop.search(req, (err, data) => {
-		res.setHeader('content-type', 'application/json')
-		res.setHeader('accepts', 'GET')
-		if (err) {
-			res.send(status.badRequest, {error: err.message})
-		} else {
-			res.send(status.ok, data)
-		}
-		res.end()
-	})
+    bookshop.search(req, (err, data) => {
+        res.setHeader('content-type', 'application/json')
+        res.setHeader('accepts', 'GET')
+        if (err) {
+            res.send(status.badRequest, {error: err.message})
+        } else {
+            res.send(status.ok, data)
+        }
+        res.end()
+    })
 })
 
 server.post('/cart', (req, res) => {
-	bookshop.addToCart(req, (err, data) => {
-		res.setHeader('content-type', 'application/json')
-		res.setHeader('accepts', 'GET, POST')
-		if (err) {
-			res.send(status.badRequest, {error: err.message})
-		} else {
-			
-			res.send(status.added, {book: data})
-		}
-		res.end()
-	})
+    bookshop.addToCart(req, (err, data) => {
+        res.setHeader('content-type', 'application/json')
+        res.setHeader('accepts', 'GET, POST')
+        if (err) {
+            res.send(status.badRequest, {error: err.message})
+        } else {
+
+            res.send(status.added, {book: data})
+        }
+        res.end()
+    })
 })
 
 server.get('/cart', (req, res) => {
-	bookshop.showCart(req, (err, data) => {
-		res.setHeader('content-type', 'application/json')
-		res.setHeader('accepts', 'GET, POST')
-		if (err) {
-			res.send(status.badRequest, {error: err.message})
-		} else {
-			res.send(status.ok, data)
-		}
-		res.end()
-	})
+    bookshop.showCart(req, (err, data) => {
+        res.setHeader('content-type', 'application/json')
+        res.setHeader('accepts', 'GET, POST')
+        if (err) {
+            res.send(status.badRequest, {error: err.message})
+        } else {
+            res.send(status.ok, data)
+        }
+        res.end()
+    })
 })
 
 server.post('/accounts', (req, res) => {
-	bookshop.addUser(req, (err, data) => {
-		res.setHeader('content-type', 'application/json')
-		res.setHeader('accepts', 'GET, POST')
-		if (err) {
-			res.send(status.badRequest, {error: err.message})
-		} else {
-			res.send(status.added, {user: data})
-		}
-		res.end()
-	})
+    bookshop.addUser(req, (err, data) => {
+        res.setHeader('content-type', 'application/json')
+        res.setHeader('accepts', 'GET, POST')
+        if (err) {
+            res.send(status.badRequest, {error: err.message})
+        } else {
+            res.send(status.added, {user: data})
+        }
+        res.end()
+    })
 })
 
 const port = process.env.PORT || defaultPort
 
 server.listen(port, err => {
-	if (err) {
-		console.error(err)
-	} else {
-		console.log('App is ready at : ' + port)
-	}
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('App is ready at : ' + port)
+    }
 })
